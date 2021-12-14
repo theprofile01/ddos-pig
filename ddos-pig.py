a9a781ff738a9b44e05fde424c2104e79ac6d9c9
theprofile01 <95331069+theprofile01@users.noreply.github.com>
2021-12-14 21:27:51 +0100

    Add files via upload

---
+++ ddos-pig.py
@@ -0,0 +1,251 @@
+import colorama
+import time
+from tkinter import *
+import tkinter
+from colorama import Fore, Back, Style
+colorama.init()
+
+print('\033[31m' + """\n\n\n\n          DDDDDDDDDDDDD      DDDDDDDDDDDDD             OOOOOOOOO        SSSSSSSSSSSSSSS                  PPPPPPPPPPPPPPPPP   IIIIIIIIII      GGGGGGGGGGGGG
+          D::::::::::::DDD   D::::::::::::DDD        OO:::::::::OO    SS:::::::::::::::S                 P::::::::::::::::P  I::::::::I   GGG::::::::::::G
+          D:::::::::::::::DD D:::::::::::::::DD    OO:::::::::::::OO S:::::SSSSSS::::::S                 P::::::PPPPPP:::::P I::::::::I GG:::::::::::::::G
+          DDD:::::DDDDD:::::DDDD:::::DDDDD:::::D  O:::::::OOO:::::::OS:::::S     SSSSSSS                 PP:::::P     P:::::PII::::::IIG:::::GGGGGGGG::::G
+            D:::::D    D:::::D D:::::D    D:::::D O::::::O   O::::::OS:::::S                               P::::P     P:::::P  I::::I G:::::G       GGGGGG
+            D:::::D     D:::::DD:::::D     D:::::DO:::::O     O:::::OS:::::S                               P::::P     P:::::P  I::::IG:::::G              
+            D:::::D     D:::::DD:::::D     D:::::DO:::::O     O:::::O S::::SSSS                            P::::PPPPPP:::::P   I::::IG:::::G              
+            D:::::D     D:::::DD:::::D     D:::::DO:::::O     O:::::O  SS::::::SSSSS     ---------------   P:::::::::::::PP    I::::IG:::::G    GGGGGGGGGG
+            D:::::D     D:::::DD:::::D     D:::::DO:::::O     O:::::O    SSS::::::::SS   -:::::::::::::-   P::::PPPPPPPPP      I::::IG:::::G    G::::::::G
+            D:::::D     D:::::DD:::::D     D:::::DO:::::O     O:::::O       SSSSSS::::S  ---------------   P::::P              I::::IG:::::G    GGGGG::::G
+            D:::::D     D:::::DD:::::D     D:::::DO:::::O     O:::::O            S:::::S                   P::::P              I::::IG:::::G        G::::G
+            D:::::D    D:::::D D:::::D    D:::::D O::::::O   O::::::O            S:::::S                   P::::P              I::::I G:::::G       G::::G
+          DDD:::::DDDDD:::::DDDD:::::DDDDD:::::D  O:::::::OOO:::::::OSSSSSSS     S:::::S                 PP::::::PP          II::::::IIG:::::GGGGGGGG::::G
+          D:::::::::::::::DD D:::::::::::::::DD    OO:::::::::::::OO S::::::SSSSSS:::::S                 P::::::::P          I::::::::I GG:::::::::::::::G
+          D::::::::::::DDD   D::::::::::::DDD        OO:::::::::OO   S:::::::::::::::SS                  P::::::::P          I::::::::I   GGG::::::GGG:::G
+          DDDDDDDDDDDDD      DDDDDDDDDDDDD             OOOOOOOOO      SSSSSSSSSSSSSSS                    PPPPPPPPPP          IIIIIIIIII      GGGGGG   GGGG\n\n\n\n\n""")
+
+target = input('\033[31m' + 'ENTER THE TARGET IP : ')
+
+if target == target:
+    print("WAITING FOR SERVER")
+    time.sleep(3)
+    print("SUCCESSFUL CONNECTION")
+    time.sleep(0.8)
+    print("SENDING REQUESTS TO", target)
+    time.sleep(0.05)
+    print("SENDING REQUESTS TO", target)
+    time.sleep(0.05)
+    print("SENDING REQUESTS TO", target)
+    time.sleep(0.05)
+    print("SENDING REQUESTS TO", target)
+    time.sleep(0.05)
+    print("SENDING REQUESTS TO", target)
+    time.sleep(0.05)
+    print("SENDING REQUESTS TO", target)
+    print("SENDING REQUESTS TO", target)
+    time.sleep(0.05)
+    print("SENDING REQUESTS TO", target)
+    time.sleep(0.05)
+    print("SENDING REQUESTS TO", target)
+    time.sleep(0.05)
+    print("SENDING REQUESTS TO", target)
+    time.sleep(0.05)
+    print("SENDING REQUESTS TO", target)
+    time.sleep(0.05)
+    print("SENDING REQUESTS TO", target)
+    print("SENDING REQUESTS TO", target)
+    time.sleep(0.05)
+    print("SENDING REQUESTS TO", target)
+    time.sleep(0.05)
+    print("SENDING REQUESTS TO", target)
+    time.sleep(0.05)
+    print("SENDING REQUESTS TO", target)
+    time.sleep(0.05)
+    print("SENDING REQUESTS TO", target)
+    time.sleep(0.05)
+    print("SENDING REQUESTS TO", target)
+    print("SENDING REQUESTS TO", target)
+    time.sleep(0.05)
+    print("SENDING REQUESTS TO", target)
+    time.sleep(0.05)
+    print("SENDING REQUESTS TO", target)
+    time.sleep(0.05)
+    print("SENDING REQUESTS TO", target)
+    time.sleep(0.05)
+    print("SENDING REQUESTS TO", target)
+    time.sleep(0.05)
+    print("SENDING REQUESTS TO", target)
+    print("SENDING REQUESTS TO", target)
+    time.sleep(0.05)
+    print("SENDING REQUESTS TO", target)
+    time.sleep(0.05)
+    print("SENDING REQUESTS TO", target)
+    time.sleep(0.05)
+    print("SENDING REQUESTS TO", target)
+    time.sleep(0.05)
+    print("SENDING REQUESTS TO", target)
+    time.sleep(0.05)
+    print("SENDING REQUESTS TO", target)
+    print("SENDING REQUESTS TO", target)
+    time.sleep(0.05)
+    print("SENDING REQUESTS TO", target)
+    time.sleep(0.05)
+    print("SENDING REQUESTS TO", target)
+    time.sleep(0.05)
+    print("SENDING REQUESTS TO", target)
+    time.sleep(0.05)
+    print("SENDING REQUESTS TO", target)
+    time.sleep(0.05)
+    print("SENDING REQUESTS TO", target)
+    print("SENDING REQUESTS TO", target)
+    time.sleep(0.05)
+    print("SENDING REQUESTS TO", target)
+    time.sleep(0.05)
+    print("SENDING REQUESTS TO", target)
+    time.sleep(0.05)
+    print("SENDING REQUESTS TO", target)
+    time.sleep(0.05)
+    print("SENDING REQUESTS TO", target)
+    time.sleep(0.05)
+    print("SENDING REQUESTS TO", target)
+    print("SENDING REQUESTS TO", target)
+    time.sleep(0.05)
+    print("SENDING REQUESTS TO", target)
+    time.sleep(0.05)
+    print("SENDING REQUESTS TO", target)
+    time.sleep(0.05)
+    print("SENDING REQUESTS TO", target)
+    time.sleep(0.05)
+    print("SENDING REQUESTS TO", target)
+    time.sleep(0.05)
+    print("SENDING REQUESTS TO", target)
+    print("SENDING REQUESTS TO", target)
+    time.sleep(0.05)
+    print("SENDING REQUESTS TO", target)
+    time.sleep(0.05)
+    print("SENDING REQUESTS TO", target)
+    time.sleep(0.05)
+    print("SENDING REQUESTS TO", target)
+    time.sleep(0.05)
+    print("SENDING REQUESTS TO", target)
+    time.sleep(0.05)
+    print("SENDING REQUESTS TO", target)
+    print("SENDING REQUESTS TO", target)
+    time.sleep(0.05)
+    print("SENDING REQUESTS TO", target)
+    time.sleep(0.05)
+    print("SENDING REQUESTS TO", target)
+    time.sleep(0.05)
+    print("SENDING REQUESTS TO", target)
+    time.sleep(0.05)
+    print("SENDING REQUESTS TO", target)
+    time.sleep(0.05)
+    print("SENDING REQUESTS TO", target)
+    print("SENDING REQUESTS TO", target)
+    time.sleep(0.05)
+    print("SENDING REQUESTS TO", target)
+    time.sleep(0.05)
+    print("SENDING REQUESTS TO", target)
+    time.sleep(0.05)
+    print("SENDING REQUESTS TO", target)
+    time.sleep(0.05)
+    print("SENDING REQUESTS TO", target)
+    time.sleep(0.05)
+    print("SENDING REQUESTS TO", target)
+    print("SENDING REQUESTS TO", target)
+    time.sleep(0.05)
+    print("SENDING REQUESTS TO", target)
+    time.sleep(0.05)
+    print("SENDING REQUESTS TO", target)
+    time.sleep(0.05)
+    print("SENDING REQUESTS TO", target)
+    time.sleep(0.05)
+    print("SENDING REQUESTS TO", target)
+    time.sleep(0.05)
+    print("SENDING REQUESTS TO", target)
+    print("SENDING REQUESTS TO", target)
+    time.sleep(0.05)
+    print("SENDING REQUESTS TO", target)
+    time.sleep(0.05)
+    print("SENDING REQUESTS TO", target)
+    time.sleep(0.05)
+    print("SENDING REQUESTS TO", target)
+    time.sleep(0.05)
+    print("SENDING REQUESTS TO", target)
+    time.sleep(0.05)
+    print("SENDING REQUESTS TO", target)
+    print("SENDING REQUESTS TO", target)
+    time.sleep(0.05)
+    print("SENDING REQUESTS TO", target)
+    time.sleep(0.05)
+    print("SENDING REQUESTS TO", target)
+    time.sleep(0.05)
+    print("SENDING REQUESTS TO", target)
+    time.sleep(0.05)
+    print("SENDING REQUESTS TO", target)
+    time.sleep(0.05)
+    print("SENDING REQUESTS TO", target)
+    print("SENDING REQUESTS TO", target)
+    time.sleep(0.05)
+    print("SENDING REQUESTS TO", target)
+    time.sleep(0.05)
+    print("SENDING REQUESTS TO", target)
+    time.sleep(0.05)
+    print("SENDING REQUESTS TO", target)
+    time.sleep(0.05)
+    print("SENDING REQUESTS TO", target)
+    time.sleep(0.05)
+    print("SENDING REQUESTS TO", target)
+    time.sleep(0.05)
+    i = 0
+    while i < 100:
+        window = Tk()
+        window.title("LOL YOU ARE SKID")
+        window.geometry("720x480")
+        window.minsize(720, 480)
+        window.config(background="#000000")
+        frame = Frame(window, bg="#000000")
+        label_title = Label(frame, text="YOU ARE A FUCKING SKID", font=("Courrier", 40), bg='black', fg='red')
+        label_title.pack()
+        frame.pack(expand=YES)
+        window.mainloop()
+        i = i +1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
